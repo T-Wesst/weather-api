2d1d76625fbc29126594f16db3cda3b3ebb9e407
T-Wesst <code.daddy17@gmail.com>
2021-10-13 21:07:24 -0700

    remove json from serverless and test get weather

--- serverless/getWeather.js
+++ serverless/getWeather.js
@@ -3,5 +3,21 @@ const baseURL = 'https://api.openweathermap.org';
 
 
 exports.handler = async event => {
-
+  // const params = JSON.parse(event.body);
+  // const {lat, lon, units} = params;
+  // const url = `${baseURL}/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${APIKEY}`;
+  try {
+  //   const weatherStream = await fetch(url);
+  //   const weatherJSON = await weatherStream.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: "getting weather from coordinates"})
+    };
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify(weatherJSON)
+    // };
+  } catch (err) {
+    return { statusCode: 422, body: err.stack };
+  }
 }
