d21693f8d2b3c4d3d1a137ac128f81e10734427f
T-Wesst <tyroojwest@gmail.com>
2021-10-27 13:02:50 -0700

    render forecast cards

--- dist/scripts/DOMFunctions.js
+++ dist/scripts/DOMFunctions.js
@@ -8,6 +8,7 @@ dayjs.extend(window.dayjs_plugin_timezone);
 
 export const buildUI = (searchText, weatherData) => {
   const { humidity, temp, wind_speed, uvi  } = weatherData.current;
+  const dailyForecast = weatherData.daily;
   let {timezone, current } = weatherData;
   let date = dayjs().tz(timezone).format('M/D/YYYY');
   let iconURL = `https://openweathermap.org/img/w/${current.weather[0].icon}.png`;
@@ -40,13 +41,13 @@ export const buildUI = (searchText, weatherData) => {
   `;
   todayContainer.innerHTML = '';
   todayContainer.append(card);
-  renderForecast(weatherData, timezone);
+  renderForecast(dailyForecast, timezone);
   }
 
-  function renderForecast(weatherData, timezone){
+  function renderForecast(dailyForecast, timezone){
+    console.log(dailyForecast);
     let startDt = dayjs().tz(timezone).add(1, 'day').startOf('day').unix();
     let endDt = dayjs().tz(timezone).add(6, 'day').startOf('day').unix();
-    const dailyForecast = weatherData.daily;
 
     let headingCol = document.createElement('div');
     headingCol.innerHTML = `<h4 class="col-12">5-Day Forecast:</h4>`;
@@ -58,6 +59,28 @@ export const buildUI = (searchText, weatherData) => {
       }
     });
   };
+
+  function renderForecastCard(day, timezone) {
+  const { humidity, wind_speed, dt: unixTs, temp: {day: temp} } = day;
+  let iconURL = `https://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+  let iconDescription = day.weather[0].description;
+  let col = document.createElement('div');
+  col.setAttribute('class', 'col-md');
+  col.classList.add('five-day-card');
+  col.innerHTML = `
+    <div class="card card-bg-primary h-100 text-white">
+      <div class="card-body p-2">
+      <h5 class="card-title">${dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY')}
+        <img src="${iconURL}" alt="${iconDescription}">
+      </h5>
+      <p class="card-text">Temp: ${temp}°F</p>
+      <p class="card-text">Wind: ${wind_speed} MPH</p>
+      <p class="card-text">Humidity: ${humidity} %</p>
+    </div>
+  </div>
+  `;  
+forecastContainer.append(col);
+};
 
 // =============== HISTORY =============== //
 let searchHistory = [];
