1e9f5002e7b34a7a4d7d7429d5ea25225bce634d
T-Wesst <tyroojwest@gmail.com>
2021-11-01 22:22:19 -0700

    test get coordinates

---
+++ tests/getCoordinates.test.js
@@ -0,0 +1,34 @@
+const getCoordinates = require('../serverless/getCoordinates');
+const mockAxios = require('axios');
+jest.mock('axios');
+
+mockAxios.get.mockImplementation(() => 
+Promise.resolve({
+  data: { 
+      coord: { 
+        lon: -118.2437, 
+        lat: 34.0522
+      },
+    },
+    statusCode: 200,
+  })
+);
+
+
+describe('getCoordinates', () => {
+  test('should return an stringified object containing longitude and latitude', async () => {
+    const event = { 
+      queryStringParameters: {
+        city: "los angeles"
+      }
+    };
+    const bodyResult ={
+        coord: { 
+          lon: -118.2437, 
+          lat: 34.0522
+        },
+    };
+    const { body } = await getCoordinates.handler(event);
+    expect(body).toBe(JSON.stringify(bodyResult));
+  })
+});
